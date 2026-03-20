77009fdccc9ca6128567baf0f353ea6547ae11f5
m1000-isa <axelisaacjimenezramirez@gmail.com>
2026-03-20 16:02:46 -0700

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(mas_length=255)
+
+    class meta:
+        ordering = ('title')
+        verbose_name_prural = 'categories'
+    
+    def __str__(self):
+        return self.title
+    
+class Post(models.model):
+
+    ACTIVATE = 'activate'
+    DRAFT = 'draft'
